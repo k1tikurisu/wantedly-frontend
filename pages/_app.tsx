2944e74e2eb06ccf86753495ae970edef7f7cdfc
Daiki Maekawa <daaikii@outlook.jp>
2021-06-23 18:17:14 +0900

    package: 📦 add Tailwind CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { DefaultSeo } from 'next-seo'
 import SEO from '../next-seo.config'
+import 'tailwindcss/tailwind.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const tailwindConfig = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = tailwindConfig
