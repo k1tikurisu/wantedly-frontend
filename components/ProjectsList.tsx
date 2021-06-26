3bdbe573d9a8124dda8ba1fad1d9d9ed732f1e49
Daiki Maekawa <daaikii@outlook.jp>
2021-06-26 23:52:57 +0900

    refactor: 💡 add early returns

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -5,16 +5,21 @@ import { ProjectsListItem } from './ProjectsListItem'
 
 export const ProjectsList: React.FC = () => {
   const { data } = useProjectsQuery()
+  const projects = data?.projects
+
+  if (!projects) return null
 
   return (
     <div className="flex flex-wrap">
-      {data?.projects?.map((project) => {
+      {projects.map((project) => {
+        if (!project) return null
+
         return (
-          <div key={project?.id} className="p-4 lg:w-1/3 w-full md:w-1/2">
+          <div key={project.id} className="p-4 lg:w-1/3 w-full md:w-1/2">
             <ProjectsListItem
-              id={project?.id}
-              title={project?.title}
-              image={project?.imageUrlLarge}
+              id={project.id}
+              title={project.title}
+              image={project.imageUrlLarge}
             />
           </div>
         )
